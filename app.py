59ebf7e771342afa8695f9c90cf6ddba33f53d6b
haujul11 <11haujul@stud.akademiet.no>
2025-10-23 08:54:05 +0200

    enkel flask app

--- app.py
+++ app.py
@@ -48,7 +48,7 @@ def kontakt():
     kontaktinfo = {
         "telefon": "92 11 17 44",
         "epost": "julian@akademiet.no",
-        "adresse": "pilestredet 1, 0981 Byen"
+        "adresse": "pilestredet 1, 0981 Oslo"
     }
     return render_template("kontakt.html", kontakt=kontaktinfo)
 
